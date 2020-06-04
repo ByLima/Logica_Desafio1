a170dd799dc9d8585c812d8a8f080d9c3ba1e528
Bianca <byanca.limass@gmail.com>
2020-06-04 15:05:58 -0300

    desafio parte 1

--- Teste1/Program.cs
+++ Teste1/Program.cs
@@ -6,7 +6,27 @@ namespace Teste1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string Nome;
+            int Idade;
+            string Sexo;
+            Console.WriteLine("Entre com o seu nome: ");
+            Console.WriteLine("Entre com a sua idade: ");
+            Console.WriteLine("Entre com a seu sexo M ou F: ");
+            Nome = Console.ReadLine();
+            Idade = Convert.ToInt32(Console.ReadLine());
+            Sexo = Console.ReadLine();
+
+            if (Idade >= 21 && Sexo == "M")
+            {
+               Console.WriteLine("Seu nome é: ",(Nome));
+                Console.WriteLine(Idade);
+                Console.WriteLine(Sexo);
+            }
+            else
+            {
+                Console.WriteLine("Menor de 21 anos ou do sexo feminino");
+            }
+            Console.ReadLine();
         }
     }
 }
